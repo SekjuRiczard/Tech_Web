f407c28192a663a357ec0811a44b8324559de28e
SekjuRiczard <106740608+SekjuRiczard@users.noreply.github.com>
2024-05-10 11:46:58 +0200

    Delete lab5/api directory

--- lab5/api/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import App from './app';
-import IndexController from './controllers/index.controller';
-import DataController from './controllers/data.controllers';
-const app: App = new App([
-    new IndexController(),
-    new DataController()
-]);
-app.listen();
